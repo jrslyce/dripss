60d9986832a8a0d5e69c50fd95543bbcdb512354
Jared Steffes <fakebusy@MacBook-Pro.local>
2024-06-16 22:28:53 -0500

    Add landing page component

---
+++ app/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div className="container">
+            <i className="fa-solid fa-right-to-bracket icon"></i>
+            <div className="login-text">Login with Twitch</div>
+        </div>
+    );
+};
+
+export default LandingPage;
